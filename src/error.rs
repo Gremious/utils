a28637137613b0d1c9a381fac9db51484ad29e24
Gremious Grenald <gremious@protonmail.com>
2021-08-11 10:46:02 +0100

    Add error mod

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use super::*;
+
+pub trait LogIfError {
+	fn log_err(self) -> Self;
+}
+
+impl<T> LogIfError for anyhow::Result<T> {
+	fn log_err(self) -> Self {
+		match &self {
+			Ok(_) => {},
+			Err(e) => log::error!("Error: {:?}", e),
+		}
+		self
+	}
+}

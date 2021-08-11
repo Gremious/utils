25d100af1dcc8a797fbf22142648abed35e15e91
Gremious Grenald <gremious@protonmail.com>
2021-08-11 10:47:05 +0100

    Add default

--- src/lib.rs
+++ src/lib.rs
@@ -73,10 +73,5 @@ impl OauthToken {
 	}
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#[must_use]
+pub fn default<T: Default>() -> T { T::default() }

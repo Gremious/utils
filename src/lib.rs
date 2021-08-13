f34d3bf40b2fa6f08ca11ee4889c451bd79ecbe8
Gremious Grenald <gremious@protonmail.com>
2021-08-13 11:04:34 +0100

    Move out serde

--- src/lib.rs
+++ src/lib.rs
@@ -7,59 +7,14 @@ use serde::{Serialize, Deserialize};
 pub mod hobo_plus;
 pub mod error;
 pub mod debug;
+pub mod serde;
 
 pub static REQWEST_CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
 
 pub fn window() -> web_sys::Window { web_sys::window().expect("no window") }
 pub fn document() -> web_sys::Document { window().document().expect("no document") }
 
-// TODO: error type
-pub trait SerdeJsonValueExt {
-	fn from_pointer<T: serde::de::DeserializeOwned>(&self, pointer: &str) -> anyhow::Result<T>;
-	fn from_pointer_mut<T: serde::de::DeserializeOwned>(&mut self, pointer: &str) -> anyhow::Result<T>;
-}
-
-impl SerdeJsonValueExt for serde_json::Value {
-	fn from_pointer<T: serde::de::DeserializeOwned>(&self, pointer: &str) -> anyhow::Result<T> {
-		use anyhow::Context;
-
-		self
-			.pointer(pointer)
-			.context(format!("missing {}", pointer))
-			.and_then(|x| Ok(serde_json::from_value(x.clone())?))
-	}
-
-	fn from_pointer_mut<T: serde::de::DeserializeOwned>(&mut self, pointer: &str) -> anyhow::Result<T> {
-		use anyhow::Context;
-
-		Ok(self
-			.pointer_mut(pointer)
-			.map(serde_json::Value::take)
-			.map(serde_json::from_value)
-			.context(format!("can't extract {}", pointer))??)
-	}
-}
-
-pub mod chrono_duration_serde {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<chrono::Duration, D::Error> {
-		Ok(chrono::Duration::seconds(i64::deserialize(deserializer)?))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &chrono::Duration, serializer: S) -> Result<S::Ok, S::Error> {
-		i64::serialize(&value.num_seconds(), serializer)
-	}
-}
-
-pub mod opt_chrono_duration_serde {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<Option<chrono::Duration>, D::Error> {
-		Ok(<Option<i64>>::deserialize(deserializer)?.map(chrono::Duration::seconds))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &Option<chrono::Duration>, serializer: S) -> Result<S::Ok, S::Error> {
-		<Option<i64>>::serialize(&value.map(|x| x.num_seconds()), serializer)
-	}
-}
-
+/* Prob don't need to keep? But also doen't a lot of things use a generic oauth token?
 #[derive(Debug, Serialize, Deserialize, Clone, smart_default::SmartDefault, PartialEq, Eq, Hash)]
 pub struct OauthToken {
 	pub expires_in: i64,
@@ -74,21 +29,10 @@ impl OauthToken {
 		(self.created_at + chrono::Duration::seconds(self.expires_in - 15)) > chrono::Utc::now()
 	}
 }
+*/
 
 pub fn spawn_complain<T>(x: impl Future<Output = anyhow::Result<T>> + 'static) {
 	spawn(async move { if let Err(e) = x.await { log::error!("{:?}", e); } });
-}
-
-#[async_trait::async_trait(?Send)]
-trait ReqwestWasmResponseExt {
-	async fn json<T: serde::de::DeserializeOwned>(self) -> anyhow::Result<T>;
-}
-
-#[async_trait::async_trait(?Send)]
-impl ReqwestWasmResponseExt for reqwest::Response {
-	async fn json<T: serde::de::DeserializeOwned>(self) -> anyhow::Result<T> {
-		Ok(serde_json::from_str(&self.text().await?)?)
-	}
 }
 
 #[must_use]
---
+++ src/serde.rs
@@ -0,0 +1,60 @@
+use super::*;
+
+// TODO: error type
+pub trait SerdeJsonValueExt {
+	fn from_pointer<T: serde::de::DeserializeOwned>(&self, pointer: &str) -> anyhow::Result<T>;
+	fn from_pointer_mut<T: serde::de::DeserializeOwned>(&mut self, pointer: &str) -> anyhow::Result<T>;
+}
+
+impl SerdeJsonValueExt for serde_json::Value {
+	fn from_pointer<T: serde::de::DeserializeOwned>(&self, pointer: &str) -> anyhow::Result<T> {
+		use anyhow::Context;
+
+		self
+			.pointer(pointer)
+			.context(format!("missing {}", pointer))
+			.and_then(|x| Ok(serde_json::from_value(x.clone())?))
+	}
+
+	fn from_pointer_mut<T: serde::de::DeserializeOwned>(&mut self, pointer: &str) -> anyhow::Result<T> {
+		use anyhow::Context;
+
+		Ok(self
+			.pointer_mut(pointer)
+			.map(serde_json::Value::take)
+			.map(serde_json::from_value)
+			.context(format!("can't extract {}", pointer))??)
+	}
+}
+
+pub mod chrono_duration_serde {
+	use serde::{Deserialize, Serialize};
+	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<chrono::Duration, D::Error> {
+		Ok(chrono::Duration::seconds(i64::deserialize(deserializer)?))
+	}
+	pub fn serialize<S: serde::Serializer>(value: &chrono::Duration, serializer: S) -> Result<S::Ok, S::Error> {
+		i64::serialize(&value.num_seconds(), serializer)
+	}
+}
+
+pub mod opt_chrono_duration_serde {
+	use serde::{Deserialize, Serialize};
+	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<Option<chrono::Duration>, D::Error> {
+		Ok(<Option<i64>>::deserialize(deserializer)?.map(chrono::Duration::seconds))
+	}
+	pub fn serialize<S: serde::Serializer>(value: &Option<chrono::Duration>, serializer: S) -> Result<S::Ok, S::Error> {
+		<Option<i64>>::serialize(&value.map(|x| x.num_seconds()), serializer)
+	}
+}
+
+#[async_trait::async_trait(?Send)]
+trait ReqwestWasmResponseExt {
+	async fn json<T: serde::de::DeserializeOwned>(self) -> anyhow::Result<T>;
+}
+
+#[async_trait::async_trait(?Send)]
+impl ReqwestWasmResponseExt for reqwest::Response {
+	async fn json<T: serde::de::DeserializeOwned>(self) -> anyhow::Result<T> {
+		Ok(serde_json::from_str(&self.text().await?)?)
+	}
+}

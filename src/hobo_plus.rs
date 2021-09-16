cf6a722707ad3b4f68d99883c5f4159806ceac02
Awpteamoose <awpteamoose@gmail.com>
2021-09-16 15:11:34 +0100

    remove global callbacks because no longer necessary

--- src/hobo_plus.rs
+++ src/hobo_plus.rs
@@ -1,56 +1,5 @@
 use super::*;
 
-// TODO: Macro, maybe even add into actual hobo? This is on window, hobo is on element so moving the mouse out while clicked doesn't trigger
-pub mod callbacks {
-	use super::*;
-
-	pub struct WindowMouseupCallback(Closure<dyn FnMut(web_sys::MouseEvent) + 'static>);
-	pub struct WindowMousedownCallback(Closure<dyn FnMut(web_sys::MouseEvent) + 'static>);
-	pub struct WindowMousemoveCallback(Closure<dyn FnMut(web_sys::MouseEvent) + 'static>);
-
-	impl WindowMouseupCallback {
-		pub fn new(mouseup: impl FnMut(web_sys::MouseEvent) + 'static) -> Self {
-			let mouseup = Closure::new(mouseup);
-			window().add_event_listener_with_callback(web_str::mouseup(), mouseup.as_ref().unchecked_ref()).unwrap();
-			Self(mouseup)
-		}
-	}
-
-	impl Drop for WindowMouseupCallback {
-		fn drop(&mut self) {
-			window().remove_event_listener_with_callback(web_str::mouseup(), self.0.as_ref().unchecked_ref()).unwrap()
-		}
-	}
-
-	impl WindowMousedownCallback {
-		pub fn new(mousedown: impl FnMut(web_sys::MouseEvent) + 'static) -> Self {
-			let mousedown = Closure::new(mousedown);
-			window().add_event_listener_with_callback(web_str::mousedown(), mousedown.as_ref().unchecked_ref()).unwrap();
-			Self(mousedown)
-		}
-	}
-
-	impl Drop for WindowMousedownCallback {
-		fn drop(&mut self) {
-			window().remove_event_listener_with_callback(web_str::mousedown(), self.0.as_ref().unchecked_ref()).unwrap();
-		}
-	}
-
-	impl WindowMousemoveCallback {
-		pub fn new(mousemove: impl FnMut(web_sys::MouseEvent) + 'static) -> Self {
-			let mousemove = Closure::new(mousemove);
-			window().add_event_listener_with_callback(web_str::mousemove(), mousemove.as_ref().unchecked_ref()).unwrap();
-			Self(mousemove)
-		}
-	}
-
-	impl Drop for WindowMousemoveCallback {
-		fn drop(&mut self) {
-			window().remove_event_listener_with_callback(web_str::mousemove(), self.0.as_ref().unchecked_ref()).unwrap();
-		}
-	}
-}
-
 /// Generic `bool` component for on-click state-switch-like events
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct Flipped(pub bool);
@@ -111,7 +60,7 @@ pub trait EleExt: Element {
 		} else {
 			self.add_component(Clicked(false));
 			self.add_on_mouse_down(move |e| { e.prevent_default(); self.get_cmp_mut::<Clicked>().0 = true; });
-			self.add_component(callbacks::WindowMouseupCallback::new(move |_| self.get_cmp_mut::<Clicked>().0 = false));
+			self.add_component(web_sys::window().unwrap().on_mouse_up(move |_| self.get_cmp_mut::<Clicked>().0 = false));
 		}
 		self
 	}

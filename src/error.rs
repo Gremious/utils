571bdb7a18c92d1b9428c6867b89e34e006098c1
Gremious Grenald <gremious@protonmail.com>
2022-03-22 11:17:41 +0000

    Log if error more

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,21 @@
 pub trait LogIfError {
-	fn log_err(self) -> Self;
+	fn log_if_err(self) -> Self;
+	fn log_if_err_with_msg(self, msg: &str) -> Self;
 }
 
-impl<T> LogIfError for anyhow::Result<T> {
-	fn log_err(self) -> Self {
+impl<T, E: std::fmt::Debug> LogIfError for anyhow::Result<T, E> {
+	fn log_if_err(self) -> Self {
 		match &self {
 			Ok(_) => {},
 			Err(e) => log::error!("Error: {:?}", e),
+		}
+		self
+	}
+
+	fn log_if_err_with_msg(self, msg: &str) -> Self {
+		match &self {
+			Ok(_) => {},
+			Err(e) => log::error!("{}: {:?}", msg, e),
 		}
 		self
 	}

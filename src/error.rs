1e8553a1ed9a7d2b0cd43c083dabaf527f1156d1
Gremious <gremious@protonmail.com>
2022-03-31 14:07:59 +0100

    Remove log_if_err_with_msg (use context instead)

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,5 @@
 pub trait LogIfError {
 	fn log_if_err(self) -> Self;
-	fn log_if_err_with_msg(self, msg: &str) -> Self;
 }
 
 impl<T, E: std::fmt::Debug> LogIfError for anyhow::Result<T, E> {
@@ -8,14 +7,6 @@ impl<T, E: std::fmt::Debug> LogIfError for anyhow::Result<T, E> {
 		match &self {
 			Ok(_) => {},
 			Err(e) => log::error!("Error: {:?}", e),
-		}
-		self
-	}
-
-	fn log_if_err_with_msg(self, msg: &str) -> Self {
-		match &self {
-			Ok(_) => {},
-			Err(e) => log::error!("{}: {:?}", msg, e),
 		}
 		self
 	}

e22dc80c37ee0f98f20ca546fdb2552e1b1cafc2
Awpteamoose <awpteamoose@gmail.com>
2022-05-26 14:17:39 +0100

    cool helpers

--- src/duration.rs
+++ src/duration.rs
@@ -7,12 +7,22 @@
 	serde::Serialize, serde::Deserialize,
 	derive_more::Display,
 	derive_more::Add, derive_more::Sub,
+	derive_more::From, derive_more::Into,
 )]
 pub struct Duration(
 	#[serde(with = "crate::serde_utils::chrono_duration")]
 	#[default(chrono::Duration::zero())]
 	chrono::Duration
 );
+
+impl Duration {
+	pub fn zero() -> Self { Self::default() }
+
+	pub fn as_seconds_f32(&self) -> f32 { self.num_milliseconds() as f32 / 1000. }
+	pub fn as_seconds_f64(&self) -> f64 { self.num_milliseconds() as f64 / 1000. }
+	pub fn seconds_f32(secs: f32) -> Self { Self(chrono::Duration::milliseconds(f32::round(secs * 1000.) as _)) }
+	pub fn seconds_f64(secs: f64) -> Self { Self(chrono::Duration::milliseconds(f64::round(secs * 1000.) as _)) }
+}
 
 impl rkyv::Archive for Duration {
 	type Archived = Duration;

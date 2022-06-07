856b3946c8e36b509496ff7640dc88006eaeb449
Awpteamoose <awpteamoose@gmail.com>
2022-06-07 15:21:01 +0100

    no longer need mmss

--- src/duration.rs
+++ src/duration.rs
@@ -45,8 +45,6 @@ impl Duration {
 	}
 	pub fn seconds_f32(secs: f32) -> Self { Self(chrono::Duration::milliseconds(f32::round(secs * 1000.) as _)) }
 	pub fn seconds_f64(secs: f64) -> Self { Self(chrono::Duration::milliseconds(f64::round(secs * 1000.) as _)) }
-
-	pub fn mmss(&self) -> String { format!("{:02}:{:02}", self.num_minutes(), self.num_seconds() % 60) }
 }
 
 impl hhmmss::Hhmmss for Duration {

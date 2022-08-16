d48123f42b7e44c00bfd7376f0a6fba7286df3e8
Gremious Grenald <gremious@protonmail.com>
2022-08-16 15:45:50 +0100

    Normalize as trait

--- src/math.rs
+++ src/math.rs
@@ -1,9 +1,13 @@
-// Maye could e a goood trait for Vec<T> or something?
-pub fn normalize(data: &mut [f32], range: std::ops::Range<f32>) {
-	let max = data.iter().max_by(|x, y| x.partial_cmp(y).unwrap_or(std::cmp::Ordering::Equal)).copied().unwrap_or(f32::EPSILON);
-	let min = data.iter().min_by(|x, y| x.partial_cmp(y).unwrap_or(std::cmp::Ordering::Equal)).copied().unwrap_or(f32::EPSILON);
-
-	// linear transformation
-	data.iter_mut().for_each(|x| { *x = range.end - range.start * ((*x - min) / (max - min)) + range.start; });
+pub trait Normalize {
+	fn normalize(&mut self, range: std::ops::Range<f32>);
 }
 
+impl Normalize for Vec<f32> {
+	fn normalize(&mut self, range: std::ops::Range<f32>) {
+		let max = self.iter().max_by(|x, y| x.partial_cmp(y).unwrap_or(std::cmp::Ordering::Equal)).copied().unwrap_or(f32::EPSILON);
+		let min = self.iter().min_by(|x, y| x.partial_cmp(y).unwrap_or(std::cmp::Ordering::Equal)).copied().unwrap_or(f32::EPSILON);
+
+		// linear transformation
+		self.iter_mut().for_each(|x| { *x = range.end - range.start * ((*x - min) / (max - min)) + range.start; });
+	}
+}

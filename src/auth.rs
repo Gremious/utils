61ef4ab296c41da1bea6b71611c41c70bf1a132b
Gremious Grenald <gremious@gremy.co.uk>
2023-06-21 14:13:45 +0100

    Simple and Complex traits

--- src/auth.rs
+++ src/auth.rs
@@ -1,5 +1,10 @@
 use super::*;
 
+// Notes:
+// Maybeee add a oauth_url() for an oatuh 2.0 flow somewhere?
+// Maybe shared::oauth::SimpleCredentials belongs here?
+// I don't want to overextend.
+
 #[derive(Debug, Serialize, Deserialize, Clone, smart_default::SmartDefault, PartialEq, Eq, Hash, rkyv::Archive, rkyv::Deserialize, rkyv::Serialize)]
 pub struct OauthToken {
 	#[serde(default = "one_hour")]
@@ -12,22 +17,13 @@ pub struct OauthToken {
 	pub created_at: chrono::DateTime<chrono::Utc>,
 }
 
-impl OauthToken {
-	pub fn fresh(&self) -> bool {
-		(self.created_at + chrono::Duration::seconds(self.expires_in - 15)) > chrono::Utc::now()
-	}
-}
-
-/// Consider also implementing this for `&mut YourAuthStruct`,
-/// because you'll likely be holding a reference to it from some state.
-pub trait AuthToken {
-	// Maybeee add a oauth_url() for an oatuh 2.0 flow?
-
-	fn expires_in(&self) -> chrono::Duration;
-	fn created_at(&self) -> chrono::DateTime<chrono::Utc>;
-	fn has_refresh_token(&self) -> bool;
-	fn access_token(&self) -> &String;
-	async fn refresh(&mut self) -> anyhow::Result<()>;
+#[allow(unused_variables)]
+pub trait SimpleToken {
+	#[must_use] fn expires_in(&self) -> chrono::Duration;
+	#[must_use] fn created_at(&self) -> chrono::DateTime<chrono::Utc>;
+	#[must_use] fn access_token(&self) -> &str;
+	#[must_use] fn refresh_token(&self) -> Option<&str> { None }
+	// fn set_refresh_token(&mut self, token: Option<&str>) {}
 
 	#[must_use] fn is_fresh(&self) -> bool {
 		let created_at = self.created_at();
@@ -36,15 +32,33 @@ pub trait AuthToken {
 
 		(created_at + expires_in - quarter_lifetime) > chrono::Utc::now()
 	}
+}
 
+impl SimpleToken for OauthToken {
+	fn expires_in(&self) -> chrono::Duration { chrono::Duration::seconds(self.expires_in) }
+	fn created_at(&self) -> chrono::DateTime<chrono::Utc> { self.created_at }
+	fn access_token(&self) -> &str { &self.access_token }
+	fn refresh_token(&self) -> Option<&str> { self.refresh_token.as_ref().map(|x| x as &str) }
+	// fn set_refresh_token(&mut self, token: Option<&str>) { if let Some(x) = token { self.refresh_token.replace(x.to_owned()); } }
+}
+
+/// Consider also implementing this for `&mut YourAuthStruct`,
+/// because you'll likely be holding a reference to it from some state.
+pub trait RefreshableToken: SimpleToken {
+	/// Refresh the token in place.
+	async fn refresh(&mut self) -> anyhow::Result<()>;
+
+	/// Refresh the token in place if not fresh.
 	async fn ensure_fresh(&mut self) -> anyhow::Result<()> {
 		if self.is_fresh() { return Ok(()); }
-		if !self.has_refresh_token() { anyhow::bail!("No refresh token."); }
+		if !self.refresh_token().is_none() { anyhow::bail!("No refresh token."); }
 		self.refresh().await
 	}
 
-	async fn fresh_token(&mut self) -> anyhow::Result<String> {
+	/// Get a fresh token.
+	async fn fresh_token(&mut self) -> anyhow::Result<&str> {
 		self.ensure_fresh().await?;
-		Ok(self.access_token().clone())
+		Ok(self.access_token())
 	}
 }
+

f359b0532ae2bd89a0d6e899cdf1efe8dda5f29f
Awpteamoose <awpteamoose@gmail.com>
2023-09-08 15:27:28 +0100

    blessed duration alhamdulillah

--- src/duration.rs
+++ src/duration.rs
@@ -3,52 +3,39 @@
 	PartialOrd, Ord,
 	PartialEq, Eq,
 	shrinkwraprs::Shrinkwrap,
-	smart_default::SmartDefault,
+	Default,
 	serde::Serialize, serde::Deserialize,
 	derive_more::Add, derive_more::Sub,
 	derive_more::From, derive_more::Into,
-
+	rkyv::Archive, rkyv::Serialize, rkyv::Deserialize,
 )]
 #[repr(transparent)]
-pub struct Duration(
-	#[serde(with = "crate::serde_utils::chrono_duration")]
-	#[default(chrono::Duration::zero())]
-	chrono::Duration,
-);
+#[archive_attr(derive(PartialOrd, Ord, PartialEq, Eq))]
+pub struct Duration(chrono::Duration);
 
 impl std::fmt::Display for Duration {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { self.0.fmt(f) }
 }
 
 impl Duration {
-	pub fn zero() -> Self { Self(chrono::Duration::zero()) }
-	pub fn max_value() -> Self { Self(chrono::Duration::max_value()) }
-	#[inline] pub fn weeks(weeks: i64) -> Self { Self(chrono::Duration::weeks(weeks)) }
-	#[inline] pub fn days(days: i64) -> Self { Self(chrono::Duration::days(days)) }
-	#[inline] pub fn hours(hours: i64) -> Self { Self(chrono::Duration::hours(hours)) }
-	#[inline] pub fn minutes(minutes: i64) -> Self { Self(chrono::Duration::minutes(minutes)) }
-	#[inline] pub fn seconds(seconds: i64) -> Self { Self(chrono::Duration::seconds(seconds)) }
+	#[inline] pub fn zero()                          -> Self { Self(chrono::Duration::zero()) }
+	#[inline] pub fn max_value()                     -> Self { Self(chrono::Duration::max_value()) }
+	#[inline] pub fn weeks(weeks: i64)               -> Self { Self(chrono::Duration::weeks(weeks)) }
+	#[inline] pub fn days(days: i64)                 -> Self { Self(chrono::Duration::days(days)) }
+	#[inline] pub fn hours(hours: i64)               -> Self { Self(chrono::Duration::hours(hours)) }
+	#[inline] pub fn minutes(minutes: i64)           -> Self { Self(chrono::Duration::minutes(minutes)) }
+	#[inline] pub fn seconds(seconds: i64)           -> Self { Self(chrono::Duration::seconds(seconds)) }
 	#[inline] pub fn milliseconds(milliseconds: i64) -> Self { Self(chrono::Duration::milliseconds(milliseconds)) }
 	#[inline] pub fn microseconds(microseconds: i64) -> Self { Self(chrono::Duration::microseconds(microseconds)) }
-	#[inline] pub fn nanoseconds(nanoseconds: i64) -> Self { Self(chrono::Duration::nanoseconds(nanoseconds)) }
+	#[inline] pub fn nanoseconds(nanoseconds: i64)   -> Self { Self(chrono::Duration::nanoseconds(nanoseconds)) }
 
-	pub fn as_seconds_f32(&self) -> f32 {
-		let secs = self.num_seconds();
-		let dur = Self::seconds(secs);
-		let nanos = *self - dur;
-		let nanos = nanos.num_nanoseconds().unwrap();
-		secs as f32 + nanos as f32 / 1_000_000_000.
-	}
-
+	pub fn as_seconds_f32(&self) -> f32 { self.as_seconds_f64() as _ }
 	pub fn as_seconds_f64(&self) -> f64 {
-		let secs = self.num_seconds();
-		let dur = Self::seconds(secs);
-		let nanos = *self - dur;
-		let nanos = nanos.num_nanoseconds().unwrap();
+		let (secs, nanos) = self.0.as_parts();
 		secs as f64 + nanos as f64 / 1_000_000_000.
 	}
-	pub fn seconds_f32(secs: f32) -> Self { Self(chrono::Duration::milliseconds(f32::round(secs * 1000.) as _)) }
-	pub fn seconds_f64(secs: f64) -> Self { Self(chrono::Duration::milliseconds(f64::round(secs * 1000.) as _)) }
+	pub fn seconds_f32(secs: f32) -> Self { Self::seconds_f64(secs as _) }
+	pub fn seconds_f64(secs: f64) -> Self { Self(chrono::Duration::nanoseconds(f64::round(secs * 1_000_000_000.) as _)) }
 
 	// https://en.wikipedia.org/wiki/Ramp_function
 	#[must_use]
@@ -86,55 +73,17 @@ impl From<std::time::Duration> for Duration {
 
 impl TryFrom<Duration> for std::time::Duration {
 	type Error = chrono::OutOfRangeError;
-
-	fn try_from(dur: Duration) -> Result<Self, Self::Error> {
-		dur.0.to_std()
-	}
+	fn try_from(dur: Duration) -> Result<Self, Self::Error> { dur.0.to_std() }
 }
 
 impl crate::hhmmss::Hhmmss for Duration { fn sms(&self) -> (i64, i64) { self.0.sms() } }
 
 impl std::ops::Mul<f32> for Duration {
 	type Output = Self;
-
-	fn mul(self, rhs: f32) -> Self::Output {
-		Self::seconds_f32(self.as_seconds_f32() * rhs)
-	}
+	fn mul(self, rhs: f32) -> Self::Output { self * rhs as f64 }
 }
 
-impl rkyv::Archive for Duration {
-	type Archived = Duration;
-	type Resolver = ((), ());
-
-	#[inline]
-	unsafe fn resolve(&self, _: usize, _: ((), ()), out: *mut Self::Archived) {
-		out.write(*self);
-	}
-}
-
-impl<D: rkyv::Fallible + ?Sized> rkyv::Deserialize<Duration, D> for rkyv::Archived<Duration> {
-	#[inline]
-	fn deserialize(&self, _: &mut D) -> Result<Duration, D::Error> {
-		Ok(*self)
-	}
-}
-
-// TODO: HACK:
-// this is kinda bad and probably UB? because chrono's Duration is not repr(transparent)
-impl<S: rkyv::Fallible + ?Sized> rkyv::Serialize<S> for Duration {
-	#[inline]
-	fn serialize(&self, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
-		rkyv::Serialize::serialize(unsafe { std::mem::transmute::<_, &(i64, i32)>(self) }, serializer)
-	}
-}
-
-#[test]
-fn rkyv_test() {
-	let duration = Duration(chrono::Duration::milliseconds(15726));
-	let bytes = rkyv::to_bytes::<_, 256>(&duration).unwrap();
-	println!("bytes: {bytes:?}");
-	let archived = unsafe { rkyv::archived_root::<Duration>(&bytes) };
-	println!("archived: {archived:?}");
-	let duration_again: Duration = rkyv::Deserialize::deserialize(archived, &mut rkyv::Infallible).unwrap();
-	assert_eq!(duration, duration_again);
+impl std::ops::Mul<f64> for Duration {
+	type Output = Self;
+	fn mul(self, rhs: f64) -> Self::Output { Self::seconds_f64(self.as_seconds_f64() * rhs) }
 }
--- src/serde_utils.rs
+++ src/serde_utils.rs
@@ -25,46 +25,6 @@ impl SerdeJsonValueExt for serde_json::Value {
 	}
 }
 
-pub mod chrono_duration_minutes {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<chrono::Duration, D::Error> {
-		Ok(chrono::Duration::minutes(i64::deserialize(deserializer)?))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &chrono::Duration, serializer: S) -> Result<S::Ok, S::Error> {
-		i64::serialize(&value.num_minutes(), serializer)
-	}
-}
-
-pub mod chrono_duration_seconds {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<chrono::Duration, D::Error> {
-		Ok(chrono::Duration::seconds(i64::deserialize(deserializer)?))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &chrono::Duration, serializer: S) -> Result<S::Ok, S::Error> {
-		i64::serialize(&value.num_seconds(), serializer)
-	}
-}
-
-pub mod chrono_duration {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<chrono::Duration, D::Error> {
-		Ok(chrono::Duration::milliseconds(i64::deserialize(deserializer)?))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &chrono::Duration, serializer: S) -> Result<S::Ok, S::Error> {
-		i64::serialize(&value.num_milliseconds(), serializer)
-	}
-}
-
-pub mod opt_chrono_duration {
-	use serde::{Deserialize, Serialize};
-	pub fn deserialize<'de, D: serde::Deserializer<'de>>(deserializer: D) -> Result<Option<chrono::Duration>, D::Error> {
-		Ok(<Option<i64>>::deserialize(deserializer)?.map(chrono::Duration::milliseconds))
-	}
-	pub fn serialize<S: serde::Serializer>(value: &Option<chrono::Duration>, serializer: S) -> Result<S::Ok, S::Error> {
-		<Option<i64>>::serialize(&value.map(|x| x.num_milliseconds()), serializer)
-	}
-}
-
 pub fn string_or_number<'de, D: serde::Deserializer<'de>>(d: D) -> Result<u64, D::Error> {
 	use serde::{Deserialize, de::Error};
 

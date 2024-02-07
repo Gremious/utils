7c7f8d5988262a0f56f2d1adc3fa698b9f7227e7
Awpteamoose <awpteamoose@gmail.com>
2024-02-07 16:26:51 +0000

    cargo update & upgrade

--- src/logger.rs
+++ src/logger.rs
@@ -11,17 +11,19 @@ pub fn setup(crate_name: &str) {
 		.format(|buf, record| {
 			use std::io::Write;
 
-			let mut dimmed = buf.style();
-			dimmed.set_color(env_logger::fmt::Color::Rgb(126, 126, 126));
+			let dimmed = anstyle::Style::new()
+				.fg_color(Some(anstyle::Color::Rgb(anstyle::RgbColor(126, 126, 126))));
+			let dimmed_reset = dimmed.render_reset();
 
-			let mut level_style = buf.style();
-			match record.level() {
-				log::Level::Trace => level_style.set_color(env_logger::fmt::Color::Rgb(169, 169, 169)),
-				log::Level::Debug => level_style.set_color(env_logger::fmt::Color::Cyan),
-				log::Level::Info => level_style.set_color(env_logger::fmt::Color::Green),
-				log::Level::Warn => level_style.set_color(env_logger::fmt::Color::Yellow),
-				log::Level::Error => level_style.set_color(env_logger::fmt::Color::Red),
-			};
+			let level_style = anstyle::Style::new()
+				.fg_color(Some(match record.level() {
+					log::Level::Trace => anstyle::Color::Rgb(anstyle::RgbColor(169, 169, 169)),
+					log::Level::Debug => anstyle::Color::Ansi(anstyle::AnsiColor::Cyan),
+					log::Level::Info => anstyle::Color::Ansi(anstyle::AnsiColor::Green),
+					log::Level::Warn => anstyle::Color::Ansi(anstyle::AnsiColor::Yellow),
+					log::Level::Error => anstyle::Color::Ansi(anstyle::AnsiColor::Red),
+				}));
+			let level_style_reset = level_style.render_reset();
 
 			let level = match record.level() {
 				log::Level::Trace => "TRACE",
@@ -31,16 +33,15 @@ pub fn setup(crate_name: &str) {
 				log::Level::Error => "ERROR",
 			};
 
-			let mut magenta = buf.style();
-			magenta.set_color(env_logger::fmt::Color::Magenta);
+			let magenta = anstyle::Style::new()
+				.fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Magenta)));
+			let magenta_reset = magenta.render_reset();
 
-			writeln!(buf, "[{time} {level} {module}] {args} {at} {file}:{line}",
-				time = dimmed.value(chrono::Utc::now().format("%Y-%m-%d %H:%M:%S%.3f")),
-				level = level_style.value(level),
-				module = magenta.value(record.module_path().unwrap_or("module?")),
-				at = dimmed.value("@"),
-				file = dimmed.value(record.file().unwrap_or("file?")),
-				line = dimmed.value(record.line().unwrap_or(0)),
+			writeln!(buf, "[{dimmed}{time}{dimmed_reset} {level_style}{level}{level_style_reset} {magenta}{module}{magenta_reset}] {args} {dimmed}@{dimmed_reset} {dimmed}{file}{dimmed_reset}:{dimmed}{line}{dimmed_reset}",
+				time = chrono::Utc::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+				module = record.module_path().unwrap_or("module?"),
+				file = record.file().unwrap_or("file?"),
+				line = record.line().unwrap_or(0),
 				args = record.args(),
 			)
 		})

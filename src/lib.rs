af7c613eeba96c53b873cedec24eec9041541ede
Awpteamoose <awpteamoose@gmail.com>
2024-08-08 13:38:30 +0100

    cooler try_json

--- src/lib.rs
+++ src/lib.rs
@@ -53,15 +53,7 @@ pub trait VerboseErrorForStatus {
 	/// Except it will log not just the status code,
 	/// but the entire json response on error.
 	/// It will also tell you which field in which sturct is missing if serde failed.
-	async fn try_json<T: for<'a> serde::Deserialize<'a>>(self) -> anyhow::Result<T>;
-
-	/// req
-	///   .error_for_status()?
-	///   .json::<serde_json::Value>().await
-	///
-	///   So that you don't do serde_json::from_value::<serde_json::Value>()
-	///   and skips the serde errors that aren't relevant
-	async fn try_json_value(self) -> anyhow::Result<serde_json::Value>;
+	async fn try_json<T: for<'a> serde::Deserialize<'a> + 'static>(self) -> anyhow::Result<T>;
 
 	/// error_for_status() but it will log the json response as well.
 	///
@@ -70,48 +62,125 @@ pub trait VerboseErrorForStatus {
 }
 
 impl VerboseErrorForStatus for reqwest::Response {
-	async fn try_json<T: for <'a> serde::Deserialize<'a>>(self) -> anyhow::Result<T> {
+	async fn try_json<T: for <'a> serde::Deserialize<'a> + 'static>(self) -> anyhow::Result<T> {
 		let status = self.status();
-		let raw_json = self.json::<serde_json::Value>().await
-			.context("Got non-json response?\nTry .text() instead of .json() and see what you get.")?;
+		let bytes = self.bytes().await?;
 		let type_name = std::any::type_name::<T>();
 
-		if status.is_success() {
-			// Can't fail since raw_json succeeded already.
-			let response_fmt = serde_json::to_string_pretty(&raw_json).unwrap();
-			let try_json = serde_json::from_value::<T>(raw_json);
-
-			Ok(try_json.map_err(anyhow::Error::from)
-				.with_context(|| format!("\nFailed to deserialize {type_name};\n\nResponse: {response_fmt}"))?)
-		} else {
-			let error = format!("Status: {}: {:?}", status.as_str(), status.canonical_reason());
-			Err(anyhow::anyhow!("{error}: \n{raw_json:#?}"))
+		if !status.is_success() {
+			if let Ok(json) = serde_json::from_slice::<serde_json::Value>(&bytes) {
+				anyhow::bail!("Status: {status}: {canonical:?}:\n{json}",
+					status = status.as_str(),
+					canonical = status.canonical_reason(),
+					json = serde_json::to_string_pretty(&json).unwrap(),
+				)
+			} else if let Ok(text) = std::str::from_utf8(&bytes) {
+				anyhow::bail!("Status: {status}: {canonical:?}:\n{text}",
+					status = status.as_str(),
+					canonical = status.canonical_reason(),
+				)
+			} else {
+				anyhow::bail!("Status: {status}: {canonical:?}: <binary>",
+					status = status.as_str(),
+					canonical = status.canonical_reason(),
+				)
+			}
 		}
-	}
 
-	async fn try_json_value(self) -> anyhow::Result<serde_json::Value> {
-		let status = self.status();
-		let raw_json = self.json::<serde_json::Value>().await
-			.context("Got non-json response?\nTry .text() instead of .json() and see what you get.")?;
-		let response_fmt = serde_json::to_string_pretty(&raw_json).unwrap();
-
-		if status.is_success() {
-			Ok(raw_json)
+		let json = match serde_json::from_slice::<serde_json::Value>(&bytes) {
+			Ok(json) => json,
+			Err(e) => match std::str::from_utf8(&bytes) {
+				Ok(text) => anyhow::bail!("Failed to parse json as {type_name}: {e}\n{text}"),
+				Err(_) => anyhow::bail!("Failed to parse json as {type_name}: {e}\n<binary>"),
+			}
+		};
+		if std::any::TypeId::of::<T>() == std::any::TypeId::of::<serde_json::Value>() {
+			let res = unsafe { std::mem::transmute_copy::<serde_json::Value, T>(&json) };
+			std::mem::forget(json);
+			Ok(res)
 		} else {
-			let error = format!("Status: {}: {:?}", status.as_str(), status.canonical_reason());
-			Err(anyhow::anyhow!("{error}: \n{response_fmt}"))
+			match serde_json::from_value(json.clone()) {
+				Ok(t) => Ok(t),
+				Err(e) => anyhow::bail!("Failed to parse json as {type_name}: {e}\n{json}",
+					type_name = type_name,
+					e = e,
+					json = serde_json::to_string_pretty(&json).unwrap(),
+				),
+			}
 		}
 	}
 
 	async fn error_for_status_with_body(self) -> anyhow::Result<reqwest::Response> {
 		let status = self.status();
 
-		if status.is_success() {
-			Ok(self)
-		} else {
-			let error = format!("Status: {}: {:?}", status.as_str(), status.canonical_reason());
-			let response = self.text().await?;
-			Err(anyhow::anyhow!("{error}: \n{response:#?}"))
-		}
+		anyhow::ensure!(status.is_success(),
+			"Status: {status}: {canonical:?}:\n{body}",
+			status = status.as_str(),
+			canonical = status.canonical_reason(),
+			body = if let Ok(body) = std::str::from_utf8(&self.bytes().await?) { body.to_owned() } else { "<binary>".to_owned() },
+		);
+
+		Ok(self)
 	}
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::test]
+async fn try_json() {
+	#[derive(serde::Deserialize)]
+	struct Foo {
+		a: i32,
+		b: String,
+		c: bool,
+	}
+
+	#[derive(serde::Deserialize)]
+	struct BadFoo {
+		d: String,
+	}
+
+	let mut server = mockito::Server::new_async().await;
+	let host = server.host_with_port();
+	let url = server.url();
+
+	server.mock("GET", "/some-text")
+		.with_status(200)
+		.with_header("content-type", "text/plain")
+		.with_body(r#"haha this not json dummy"#)
+		.create_async().await;
+	server.mock("GET", "/some-json")
+		.with_status(200)
+		.with_header("content-type", "application/json")
+		.with_body(r#"{"a":1}"#)
+		.create_async().await;
+	server.mock("GET", "/foo-json")
+		.with_status(200)
+		.with_header("content-type", "application/json")
+		.with_body(r#"{"a":1,"b":"ahha","c":true}"#)
+		.create_async().await;
+	server.mock("GET", "/400-with-text")
+		.with_status(400)
+		.with_header("content-type", "text/plain")
+		.with_body(r#"error text"#)
+		.create_async().await;
+	server.mock("GET", "/400-with-json")
+		.with_status(400)
+		.with_header("content-type", "application/json")
+		.with_body(r#"{"error":"error text"}"#)
+		.create_async().await;
+
+	assert!(reqwest::get(&format!("{url}/some-text")).await.unwrap()
+		.try_json::<serde_json::Value>().await.is_err());
+	assert!(reqwest::get(&format!("{url}/some-text")).await.unwrap()
+		.try_json::<Foo>().await.is_err());
+	assert!(reqwest::get(&format!("{url}/some-json")).await.unwrap()
+		.try_json::<serde_json::Value>().await.is_ok());
+	assert!(reqwest::get(&format!("{url}/foo-json")).await.unwrap()
+		.try_json::<Foo>().await.is_ok());
+	assert!(reqwest::get(&format!("{url}/foo-json")).await.unwrap()
+		.try_json::<BadFoo>().await.is_err());
+	assert!(reqwest::get(&format!("{url}/400-with-text")).await.unwrap()
+		.try_json::<Foo>().await.is_err());
+	assert!(reqwest::get(&format!("{url}/400-with-json")).await.unwrap()
+		.try_json::<Foo>().await.is_err());
 }

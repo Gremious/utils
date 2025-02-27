7115b286ce3f1bf638b07e812d08d9e866e09e9c
Awpteamoose <awpteamoose@gmail.com>
2025-02-27 11:48:15 +0000

    custom sugars

--- src/lib.rs
+++ src/lib.rs
@@ -93,6 +93,49 @@ pub fn debugger() {
 	web_sys::js_sys::eval("debugger").ok();
 }
 
+#[macro_export]
+macro_rules! dur {
+	// Base case: when we've collected all tokens except the unit
+	(@collect ($($amount:tt)*) sec) => { std::time::Duration::from_secs_f64(($($amount)*) as f64) };
+	(@collect ($($amount:tt)*) min) => { std::time::Duration::from_secs_f64(($($amount)*) as f64 * 60.) };
+	(@collect ($($amount:tt)*) h) => { std::time::Duration::from_secs_f64(($($amount)*) as f64 * 3600.) };
+	(@collect ($($amount:tt)*) ms) => { std::time::Duration::from_millis(($($amount)*) as u64) };
+	
+	// Recursive case: collect tokens until we hit the unit
+	(@collect ($($amount:tt)*) $next:tt $($rest:tt)*) => { $crate::dur!(@collect ($($amount)* $next) $($rest)*) };
+	
+	// Entry point: start collecting tokens
+	($first:tt $($tokens:tt)*) => { $crate::dur!(@collect ($first) $($tokens)*) };
+}
+
+// https://veykril.github.io/tlborm/decl-macros/building-blocks/counting.html#bit-twiddling
+#[macro_export]
+macro_rules! count {
+	() => { 0 };
+	($odd:tt $($a:tt $b:tt)*) => { ($crate::count!($($a)*) << 1) | 1 };
+	($($a:tt $even:tt)*) => { $crate::count!($($a)*) << 1 };
+}
+
+#[macro_export]
+macro_rules! hmap {
+	() => { ::std::collections::HashMap::new() };
+	($($key:expr => $value:expr),+ $(,)?) => {{
+		let mut map = ::std::collections::HashMap::with_capacity($crate::count!($($key)*));
+		$(let _ = map.insert($key, $value);)+
+		map
+	}};
+}
+
+#[macro_export]
+macro_rules! hset {
+	() => { ::std::collections::HashSet::new() };
+	($($value:expr),+ $(,)?) => {{
+		let mut set = ::std::collections::HashSet::with_capacity($crate::count!($($value)*));
+		$(let _ = set.insert($value);)+
+		set
+	}};
+}
+
 #[extend::ext(pub, name = VerboseErrorForStatus)]
 impl reqwest::Response {
 	/// Basically
